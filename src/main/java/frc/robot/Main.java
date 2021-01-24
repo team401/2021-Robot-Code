434fe86eb7bd9c0268945c6324d815139354f67d
nicholasstoll <53276161+nicholasstoll@users.noreply.github.com>
2021-01-23 22:32:31 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Main.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+
+    private Main() {}
+
+    public static void main(String... args) {
+
+        RobotBase.startRobot(Robot::new);
+
+      }
+      
+}
