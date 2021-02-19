2c8d8a62b98c534b62330fcc4b0e2b2be109d9e6
annekke <annkvgelder@gmail.com>
2021-02-19 18:47:43 -0500

    commit

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -32,6 +32,12 @@ public class IntakeSubsystem extends SubsystemBase {
 
     }
 
+    public void stopIntakeMotor() {
+
+        intakeMotor.set(0);
+
+    }
+
     public void extendIntake() {
 
         leftIntakeSolenoid.set(Value.kForward);
