0f04feeb96d4ef20df684037fc573bf6378bea17
nicholasstoll <nicholasstoll@mcps.org>
2021-02-20 15:54:59 -0500

    very helpful changes x100

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.DriveSubsystem;
 
 public class Robot extends TimedRobot {
     
