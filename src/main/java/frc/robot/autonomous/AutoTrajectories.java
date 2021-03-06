d4e7c94e1177c51a43041dbd01513ad92d1b3cf0
Ayca <ayca@bulbul.net>
2021-03-05 20:35:27 -0500

    holonomic timeeeee

--- src/main/java/frc/robot/autonomous/AutoTrajectories.java
+++ src/main/java/frc/robot/autonomous/AutoTrajectories.java
@@ -15,52 +15,54 @@ import frc.robot.Constants.DriveConstants;
 
 public class AutoTrajectories {
 
-    private final static TrajectoryConfig config = 
-        new TrajectoryConfig(
+    private static TrajectoryConfig config;
+
+    public static Trajectory autoNavSlalomTrajectory;
+
+    // preliminary pathing for barrel
+    public static Trajectory autoNavBarrelTrajectory;
+
+    public AutoTrajectories() {
+        config = new TrajectoryConfig(
             AutoConstants.maxVelMetersPerSec, 
             AutoConstants.maxAccelMetersPerSecondSq
-        )
-        .setKinematics(DriveConstants.kinematics);
+        ).setKinematics(DriveConstants.kinematics);
 
-    public static Trajectory autoNavSlalomTrajectory = 
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(30), new Rotation2d(0)), 
-            List.of(
-                new Translation2d(Units.inchesToMeters(80), Units.inchesToMeters(60)),
-                new Translation2d(Units.inchesToMeters(120), Units.inchesToMeters(90)),    
-                new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(90)),
-                new Translation2d(Units.inchesToMeters(270), Units.inchesToMeters(60)),     
-                new Translation2d(Units.inchesToMeters(300), Units.inchesToMeters(30)),
-                new Translation2d(Units.inchesToMeters(330), Units.inchesToMeters(60)),
-                new Translation2d(Units.inchesToMeters(300), Units.inchesToMeters(90)),
-                new Translation2d(Units.inchesToMeters(280), Units.inchesToMeters(60)),
-                new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(20)),
-                new Translation2d(Units.inchesToMeters(120), Units.inchesToMeters(20)),
-                new Translation2d(Units.inchesToMeters(90), Units.inchesToMeters(55))
-            ),
-            new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(75), new Rotation2d(0)), 
-            config
-        );
+        config.setStartVelocity(0.0);
+        config.setEndVelocity(0.0);
 
-            //preliminary pathing for barrel
-    public static Trajectory autoNavBarrelTrajectory = 
-        TrajectoryGenerator.generateTrajectory(
+        autoNavSlalomTrajectory = TrajectoryGenerator.generateTrajectory(
+            new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(30), new Rotation2d(0)),
+            List.of(new Translation2d(Units.inchesToMeters(80), Units.inchesToMeters(60)),
+                    new Translation2d(Units.inchesToMeters(120), Units.inchesToMeters(90)),
+                    new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(90)),
+                    new Translation2d(Units.inchesToMeters(270), Units.inchesToMeters(60)),
+                    new Translation2d(Units.inchesToMeters(300), Units.inchesToMeters(30)),
+                    new Translation2d(Units.inchesToMeters(330), Units.inchesToMeters(60)),
+                    new Translation2d(Units.inchesToMeters(300), Units.inchesToMeters(90)),
+                    new Translation2d(Units.inchesToMeters(280), Units.inchesToMeters(60)),
+                    new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(20)),
+                    new Translation2d(Units.inchesToMeters(120), Units.inchesToMeters(20)),
+                    new Translation2d(Units.inchesToMeters(90), Units.inchesToMeters(55))),
+            new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(75), new Rotation2d(0)), config);
+
+        autoNavBarrelTrajectory = TrajectoryGenerator.generateTrajectory(
             new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(90), new Rotation2d(0)),
-            List.of(
-                new Translation2d(Units.inchesToMeters(150), Units.inchesToMeters(90)),
-                new Translation2d(Units.inchesToMeters(160), Units.inchesToMeters(48)),
-                new Translation2d(Units.inchesToMeters(120), Units.inchesToMeters(60)),
-                new Translation2d(Units.inchesToMeters(150), Units.inchesToMeters(90)),
-                new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(90)),
-                new Translation2d(Units.inchesToMeters(250), Units.inchesToMeters(135)),
-                new Translation2d(Units.inchesToMeters(210), Units.inchesToMeters(110)),
-                new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(80)),
-                new Translation2d(Units.inchesToMeters(295), Units.inchesToMeters(30)),
-                new Translation2d(Units.inchesToMeters(295), Units.inchesToMeters(80)),
-                new Translation2d(Units.inchesToMeters(100), Units.inchesToMeters(80))
-            ), 
-            new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(80), new Rotation2d(0)),
-            config
-        );
+            List.of(new Translation2d(Units.inchesToMeters(150), Units.inchesToMeters(90)),
+                    new Translation2d(Units.inchesToMeters(160), Units.inchesToMeters(48)),
+                    new Translation2d(Units.inchesToMeters(120), Units.inchesToMeters(60)),
+                    new Translation2d(Units.inchesToMeters(150), Units.inchesToMeters(90)),
+                    new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(90)),
+                    new Translation2d(Units.inchesToMeters(250), Units.inchesToMeters(135)),
+                    new Translation2d(Units.inchesToMeters(210), Units.inchesToMeters(110)),
+                    new Translation2d(Units.inchesToMeters(240), Units.inchesToMeters(80)),
+                    new Translation2d(Units.inchesToMeters(295), Units.inchesToMeters(30)),
+                    new Translation2d(Units.inchesToMeters(295), Units.inchesToMeters(61.5)),
+                    new Translation2d(Units.inchesToMeters(100), Units.inchesToMeters(61.5))),
+            new Pose2d(Units.inchesToMeters(48.5), Units.inchesToMeters(61.5), new Rotation2d(0)), config);
+    }
+
+
 
 }
+
--- src/main/java/frc/robot/commands/drivetrain/FollowTrajectory.java
+++ src/main/java/frc/robot/commands/drivetrain/FollowTrajectory.java
@@ -2,54 +2,113 @@ package frc.robot.commands.drivetrain;
 
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 
-import javax.xml.namespace.QName;
-
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.HolonomicDriveController;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class FollowTrajectory extends SwerveControllerCommand {
+public class FollowTrajectory extends CommandBase {
 
     private final Trajectory trajectory;
 
+    private Timer time = new Timer();;
+
+    private final SwerveDriveKinematics kinematics = DriveConstants.kinematics;
+
+    private final DriveSubsystem drive;
+
+    private final Pose2d tolerance = 
+        new Pose2d(
+            Units.inchesToMeters(0.5), 
+            Units.inchesToMeters(0.5), 
+            new Rotation2d(0)
+        );
+
     private static final ProfiledPIDController rotationController = 
         new ProfiledPIDController(2, 0, 0,
-            new TrapezoidProfile.Constraints(AutoConstants.maxVelMetersPerSec,
+            new TrapezoidProfile.Constraints(
+                AutoConstants.maxVelMetersPerSec,
                 AutoConstants.maxAccelMetersPerSecondSq
             )
         );
 
-    static {
+    private static final HolonomicDriveController controller = 
+        new HolonomicDriveController(
+            new PIDController(1.1, 0, 0.2), 
+            new PIDController(1.1, 0, 0.2), 
+            rotationController
+        );
+
+    public FollowTrajectory(DriveSubsystem drive, Trajectory trajectory) {
+
+        this.trajectory = trajectory;
+        this.drive = drive;
+        
+        controller.setTolerance(tolerance);
 
         rotationController.enableContinuousInput(-Math.PI, Math.PI);
 
     }
-
-    public FollowTrajectory(DriveSubsystem drive, Trajectory trajectory) {
-
-        super(
-            trajectory, 
-            drive::getPose, 
-            DriveConstants.kinematics, 
-            new PIDController(1.1, 0, 0.2),
-            new PIDController(1.1, 0, 0.2), 
-            rotationController,
-            drive::setModuleStates, 
-            drive
-        );
-
-        this.trajectory = trajectory;
     
+    public Pose2d getInitialPose() {
+
+        return trajectory.getInitialPose();
+
     }
 
-    public Pose2d getInitialPose() {
-        
-        return trajectory.getInitialPose();
+    public Pose2d getCurrentPose() {
+
+        return drive.getPose();
+
+    }
+
+    @Override
+    public void initialize() {
+
+        time.reset();
+        time.start();
+
+    }
+
+    @Override
+    public void execute() {
+
+        Trajectory.State goal = trajectory.sample(time.get());
+
+        ChassisSpeeds adjustedSpeeds = 
+            controller.calculate(
+                getCurrentPose(), 
+                goal, 
+                Rotation2d.fromDegrees(0.0)
+            );
+
+        SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(adjustedSpeeds);
+        drive.setModuleStates(moduleStates);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+        time.stop();
+
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return time.hasElapsed(trajectory.getTotalTimeSeconds());
 
     }
 
