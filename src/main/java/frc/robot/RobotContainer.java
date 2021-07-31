50b5e1872c2897008f340960faf4b39be4f7fb86
Nicholas Stoll <nicholasstoll@mcps.org>
2021-07-31 12:19:43 -0400

    Changes for demoing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,34 +4,17 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.InputDevices;
-import frc.robot.commands.drivetrain.QuickTurn;
-import frc.robot.commands.drivetrain.AlignWithVisionTarget;
-import frc.robot.commands.drivetrain.DriveTime;
-import frc.robot.commands.drivetrain.FollowTrajectory;
-import frc.robot.commands.autonomous.InfiniteRecharge2021Auto;
-import frc.robot.commands.autonomous.InfiniteRecharge2021Auto.IntakeSource;
-import frc.robot.commands.autonomous.InfiniteRecharge2021Auto.StartingPosition;
-import frc.robot.commands.climbing.ActuateClimbers;
 import frc.robot.commands.drivetrain.OperatorControl;
 import frc.robot.commands.superstructure.indexing.Waiting;
-import frc.robot.commands.superstructure.shooting.RampUpToSpeed;
-import frc.robot.commands.superstructure.shooting.RampUpWithVision;
 import frc.robot.commands.superstructure.shooting.Shoot;
 import frc.robot.subsystems.IndexingSubsystem;
-import frc.robot.subsystems.ClimbingSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
@@ -49,54 +32,36 @@ public class RobotContainer {
     private final IntakeSubsystem intake = new IntakeSubsystem();
     private final IndexingSubsystem indexer = new IndexingSubsystem();
     private final ShooterSubsystem shooter = new ShooterSubsystem();
-    private final VisionSubsystem limelight = new VisionSubsystem();
-    private final ClimbingSubsystem climber = new ClimbingSubsystem();
     
-    SendableChooser<Command> autoSelector = new SendableChooser<Command>();
-
     public RobotContainer() {
-
-        SmartDashboard.putNumber("desired RPM", 0);
 
         drive.setDefaultCommand(
             new OperatorControl(
                 drive,
-                () -> leftJoystick.getY(GenericHID.Hand.kLeft),
-                () -> leftJoystick.getX(GenericHID.Hand.kLeft),
-                () -> rightJoystick.getX(GenericHID.Hand.kRight),
+                () -> leftJoystick.getY(GenericHID.Hand.kLeft) / 2,
+                () -> leftJoystick.getX(GenericHID.Hand.kLeft) / 2,
+                () -> rightJoystick.getX(GenericHID.Hand.kRight) / 2,
                 true
-            )
-        );
-
-        climber.setDefaultCommand(
-            new ActuateClimbers(
-                climber,
-                () -> gamepad.getRawAxis(5)
             )
         );
 
         indexer.setDefaultCommand(new Waiting(indexer));
 
-        configureButtonBindings();
-
-        autoSelector.setDefaultOption("Do nothing", new InstantCommand());
-        autoSelector.addOption("Start right to trench right", new InfiniteRecharge2021Auto(StartingPosition.Right, IntakeSource.TrenchRight, drive, intake, indexer, limelight, shooter));
-        autoSelector.addOption("Start right 3 ball", new InfiniteRecharge2021Auto(StartingPosition.Right, IntakeSource.NoSource, drive, intake, indexer, limelight, shooter));
-        autoSelector.addOption("Start mid 3 ball", new InfiniteRecharge2021Auto(StartingPosition.Mid, IntakeSource.NoSource, drive, intake, indexer, limelight, shooter));
-        autoSelector.addOption("Start left 3 ball", new InfiniteRecharge2021Auto(StartingPosition.Left, IntakeSource.NoSource, drive, intake, indexer, limelight, shooter));
-
-        SmartDashboard.putData(autoSelector);
+        shooter.setDefaultCommand(
+            new InstantCommand(
+                () -> shooter.runShooterPercent(gamepad.getRawAxis(3) / 5), 
+                shooter
+            )
+        );
 
     }
 
     public void configureButtonBindings() {
 
-        // intake
+        // run intake
         new JoystickButton(gamepad, Button.kB.value)
             .whenPressed(new InstantCommand(intake::runIntakeMotor))
-            //.alongWith(new InstantCommand(intake::extendIntake)))
             .whenReleased(new InstantCommand(intake::stopIntakeMotor));
-            //.alongWith(new InstantCommand(intake::retractIntake)));
 
         // shoot
         new JoystickButton(gamepad, Button.kY.value)
@@ -123,51 +88,18 @@ public class RobotContainer {
                 )
             );
 
-        // align with vision
-        new JoystickButton(leftJoystick, Joystick.ButtonType.kTop.value)
-            .whileHeld(new AlignWithVisionTarget(drive, limelight));
-         
-        // quick turn to 180 degrees
-        new JoystickButton(rightJoystick, Joystick.ButtonType.kTrigger.value)
-            .whileHeld(new QuickTurn(Math.PI, drive));
-
-        // quick turn to 0 degrees
-        new JoystickButton(leftJoystick, Joystick.ButtonType.kTrigger.value) 
-            .whileHeld(new QuickTurn(0, drive));
-
         // reset imu 
         new JoystickButton(rightJoystick, 3)
             .whenPressed(new InstantCommand(drive::resetImu));
 
-        // ramp up with vision using regression
-        //new JoystickButton(gamepad, Button.kBumperLeft.value)
-        //    .whileHeld(new RampUpWithVision(shooter, limelight))
-        //    .whenReleased(new InstantCommand(() -> shooter.runShooterPercent(0)));
-
-        // deploy climbers
-        new JoystickButton(gamepad, Button.kX.value) 
-            .whenPressed(new InstantCommand(climber::deployClimbers))
-            .whenPressed(new InstantCommand(intake::extendIntake));
-
-        new JoystickButton(gamepad, Button.kA.value) 
-            .whenPressed(new InstantCommand(climber::lockClimbers))
-            .whenPressed(new InstantCommand(intake::retractIntake));
-
-        new JoystickButton(gamepad, Button.kBumperRight.value)
-            .whileHeld(new RunCommand(() -> shooter.runVelocityProfileController(Units.rotationsPerMinuteToRadiansPerSecond(4200))))
-            .whenReleased(new InstantCommand(() -> shooter.stopShooter()));
-
+        // toggle intake up/down
         new JoystickButton(gamepad, Button.kBumperLeft.value)
             .whenPressed(new InstantCommand(intake::toggleIntake));
     }
 
     public Command getAutonomousCommand() {
 
-        drive.resetImu();
-
-        return new RunCommand(() -> shooter.runVelocityProfileController(Units.rotationsPerMinuteToRadiansPerSecond(4100))).withTimeout(3)
-
-        .andThen(new Shoot(shooter, indexer));
+        return null;
         
     }
 
