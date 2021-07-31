afb6e8a25e8aefef47d7d920c9a5abfc4acb074d
Nicholas Stoll <nicholasstoll@mcps.org>
2021-07-31 15:31:37 -0400

    Changing shooter power to not crit any children

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,14 +4,15 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.InputDevices;
 import frc.robot.commands.drivetrain.OperatorControl;
 import frc.robot.commands.superstructure.indexing.Waiting;
-import frc.robot.commands.superstructure.shooting.Shoot;
 import frc.robot.subsystems.IndexingSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -38,9 +39,9 @@ public class RobotContainer {
         drive.setDefaultCommand(
             new OperatorControl(
                 drive,
-                () -> leftJoystick.getY(GenericHID.Hand.kLeft) / 2,
-                () -> leftJoystick.getX(GenericHID.Hand.kLeft) / 2,
-                () -> rightJoystick.getX(GenericHID.Hand.kRight) / 2,
+                () -> leftJoystick.getY(GenericHID.Hand.kLeft),
+                () -> leftJoystick.getX(GenericHID.Hand.kLeft),
+                () -> rightJoystick.getX(GenericHID.Hand.kRight),
                 true
             )
         );
@@ -48,27 +49,32 @@ public class RobotContainer {
         indexer.setDefaultCommand(new Waiting(indexer));
 
         shooter.setDefaultCommand(
-            new InstantCommand(
-                () -> shooter.runShooterPercent(gamepad.getRawAxis(3) / 5), 
+            new RunCommand(
+                () -> shooter.runShooterPercent(gamepad.getRawAxis(3) / 2.75), 
                 shooter
             )
         );
+
+        configureButtonBindings();
 
     }
 
     public void configureButtonBindings() {
 
-        // run intake
+        // intake
         new JoystickButton(gamepad, Button.kB.value)
             .whenPressed(new InstantCommand(intake::runIntakeMotor))
             .whenReleased(new InstantCommand(intake::stopIntakeMotor));
 
         // shoot
         new JoystickButton(gamepad, Button.kY.value)
-            .whileHeld(new Shoot(shooter, indexer))
+            .whileHeld(
+                new InstantCommand(shooter::runKicker)
+                .alongWith(new InstantCommand(indexer::runConveyor, indexer))
+            )
             .whenReleased(
-                new InstantCommand(shooter::stopShooter)
-                .alongWith(new InstantCommand(indexer::stopConveyor))
+                new InstantCommand(shooter::stopKicker)
+                .alongWith(new InstantCommand(indexer::stopConveyor, indexer))
             );
 
         // manual reverse
@@ -88,11 +94,18 @@ public class RobotContainer {
                 )
             );
 
+        new JoystickButton(gamepad, Button.kBumperRight.value)
+            .whileHeld(
+                new InstantCommand(
+                    () -> shooter.runVelocityProfileController(Units.rotationsPerMinuteToRadiansPerSecond(2500)))
+            )
+            .whenReleased(new InstantCommand(shooter::stopShooter));
+
         // reset imu 
         new JoystickButton(rightJoystick, 3)
             .whenPressed(new InstantCommand(drive::resetImu));
 
-        // toggle intake up/down
+        // toggle intake
         new JoystickButton(gamepad, Button.kBumperLeft.value)
             .whenPressed(new InstantCommand(intake::toggleIntake));
     }
